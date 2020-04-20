d3488832c4270b4acebcfa3d0873edd564eee8aa
wanghongwei2 <wanghongwei2@100tal.com>
2020-04-20 09:13:36 +0800

    node

--- src/node.go
+++ src/node.go
@@ -1,1 +1,13 @@
 package src
+
+type Node struct {
+	Key string `json:"key"`
+	Score int64 `json:"score"`
+	Value interface{} `json:"value"`
+}
+
+type ListNode struct {
+	Value Node `json:"value"`
+	Pre *ListNode
+	Next *ListNode
+}
